29dcfe77b48ec2d29e176a18948a5640e0b814fb
John Coggeshall <john@coggeshall.org>
2016-09-03 19:55:03 -0400

    initial commit

---
+++ app/config/packages/cooglemirror/smappee/widget.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'placement' => 'top_left',
+    'smappee' => [
+        'client_id' => 'coogle',
+        'client_secret' => 'RT9DQqD6ST',
+        'username' => 'coogle',
+        'password' => '4-Ph0enix'
+    ],
+    'model' => 'Cooglemirror\Smappee\UsageRecord' 
+];
